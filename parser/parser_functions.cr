2a6738b0df3aec8884610aadfa52b910fbdaa245
jurreM-dev <269440156+JurreM-dev@users.noreply.github.com>
2026-09-13 18:41:51 +0100

    delete unnecessary file

--- parser/parser_functions.cr
+++
@@ -1,8 +0,0 @@
-def eat(expectedType, tokens, index : Int32)
-    if(!tokens[index] || tokens[index][:type] != expectedType)
-     puts "error"
-     {tokens[index], index + 1}
-    else 
-    {tokens[index], index + 1}
-    end
-end
