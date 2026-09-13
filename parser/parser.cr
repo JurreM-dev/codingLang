d7eaf90848f6c48317019c853c0a4a492c7a4c34
jurreM-dev <269440156+JurreM-dev@users.noreply.github.com>
2026-09-13 18:41:14 +0100

    refactor the parser

--- parser/parser.cr
+++ parser/parser.cr
@@ -1,48 +1,87 @@
-require "./parser_functions"
-
 alias AST_hash = Hash(String, String | Int32)
-def parse(tokens, errLogger) 
-  ast = Array(AST_hash).new
-  index = 1
-  while index < tokens.size
-    case tokens[index][:value]
+
+class Parser
+  @tokens : Array(NamedTuple(type: String, value: String))
+  @errLogger : ErrorLogger
+  def initialize(tokens, errLogger)
+    @tokens = tokens
+    @index = 1
+    @errLogger = errLogger
+  end
   
-    when "printf"
-      _, index = eat("IDENTIFIER", tokens, index)
-      _, index = eat("LPARA", tokens, index)
-      used_value, index = eat("STRING", tokens, index)
-      _, index = eat("RPARA", tokens, index)
-      value_current = used_value[:value]
-      ast_item = AST_hash{"type" => "printing", "value" => value_current}
-      ast << ast_item
+  def parse
+    ast = Array(AST_hash).new
+
+    while @index < @tokens.size
+      case @tokens[@index][:value]
   
-    when "int"
-      _, index = eat("IDENTIFIER", tokens, index)
-      _, index = eat("COLON", tokens, index)
-      name, index = eat("IDENTIFIER", tokens, index)
-      _, index = eat("EQUALS", tokens, index)
-      used_value, index = eat("NUM", tokens, index)
-      varName = name[:value]
-      value_current = used_value[:value]
-      value_current = value_current.to_i
-      if value_current.is_a?(Int32)
-        ast_item = AST_hash{"type" => "variableDeclarationInt", "name" => varName, "value" => value_current}
+      when "printf"
+        eat("IDENTIFIER")
+        eat("LPARA")
+        used_value = eat("STRING")
+        eat("RPARA")
+
+        value_current = used_value[:value]
+        ast_item = AST_hash{"type" => "printing", "value" => value_current}
         ast << ast_item
+  
+      when "int"
+        eat("IDENTIFIER")
+        eat("COLON")
+        name = eat("IDENTIFIER")
+        eat("EQUALS")
+        used_value = eat("NUM")
+
+        varName = name[:value]
+        value_current = used_value[:value]
+        value_current = value_current.to_i
+
+        if value_current.is_a?(Int32)
+          ast_item = AST_hash{
+            "type" => "variableDeclarationInt",
+            "name" => varName,
+            "value" => value_current
+          }
+          ast << ast_item
+        else
+          @errLogger.add_error(
+            "ERROR, expected an intenger but got: #{typeof(value_current)}\nlunarMyth, parser index:#{@index}"
+          )
+        end
+
+      when "readOutIntVar"
+        eat("IDENTIFIER")
+        eat("LPARA")
+        variableData = eat("IDENTIFIER")
+        eat("RPARA")
+
+        variableName = variableData[:value]
+        ast_item = AST_hash{"type" => "readIntVariable", "value" => variableName}
+        ast << ast_item
+
       else
-        errLogger.add_error("ERROR, expected an intenger but got: #{typeof(value_current)}\nlunarMyth, parser index:#{index}")
+        @errLogger.add_error(
+          "ERROR, #{@tokens[@index][:value]} was not found as a valid statement\nlunarMyth, parser index:#{@index}\n\n"
+        )
+        @index += 1
       end
-    when "readOutIntVar"
-      _, index = eat("IDENTIFIER", tokens, index)
-      _, index = eat("LPARA", tokens, index)
-      variableData, index = eat("IDENTIFIER", tokens, index)
-      _, index = eat("RPARA", tokens, index)
-      variableName = variableData[:value]
-      ast_item = AST_hash{"type" => "readIntVariable", "value" => variableName}
-      ast << ast_item
-    else 
-      errLogger.add_error("ERROR, #{tokens[index][:value]} was not found as a valid statement\nlunarMyth, parser index:#{index}\n\n")
-      index += 1
+    end
+
+    return ast
+  end
+
+  def eat(expectedType)
+    if !@tokens[@index] || @tokens[@index][:type] != expectedType
+      @errLogger.add_error(
+        "ERROR, expected type #{expectedType}\nlunarMyth, parser index:#{@index}"
+      )
+      token = @tokens[@index]
+      @index += 1
+      return token
+    else
+      token = @tokens[@index]
+      @index += 1
+      return token
     end
   end
-  return ast
 end
--- runner.cr
+++ runner.cr
@@ -4,6 +4,7 @@ require "./executors/interpreter"
 require "./errors"
 
 errLogger = ErrorLogger.new
-
-evaluate(parse(tokenize("printf('hi') printf('yoski') int: age = 20 int: day = 201 readOutIntVar(day)"), errLogger))
-errLogger.error_log()
+tokens = tokenize("printf('hi') printf('yoski') int: age = 20 int: day = 201 readOutIntVar(day)")
+parser = Parser.new(tokens, errLogger)
+ast = parser.parse
+evaluate(ast)
