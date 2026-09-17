a439c29e2c718a20f616a0f88446feb01ff0afa2
jurreM-dev <269440156+JurreM-dev@users.noreply.github.com>
2026-09-17 22:19:52 +0100

    make a bit more complex typing system in saves

--- saveSystemExperimental/test.cr
+++ saveSystemExperimental/test.cr
@@ -1,8 +1,12 @@
+alias Save_item = Hash(String, String|Int32)
+
 puts "do you want to save or load? "
 user_input = gets
 if(user_input == "save") 
   saveName = "testSave"
-  savedVars = ["name = jeff", "age = 20"]
+  savedVars = Array(Save_item).new
+  item = Save_item{"type" => "Int", "name" => "age", "value" => 20}
+  savedVars << item
   content = prepareSave(saveName, savedVars)
   File.write(".localSave.txt", content)
 else
@@ -12,7 +16,7 @@ end
 def prepareSave(saveName : String, vars)
   saveValue = "[#{saveName}]\n"
   vars.each do |var|
-    saveValue += var + "\n"
+  saveValue += "#{var["name"]}:#{var["type"]}=#{var["value"]}"
   end
   return saveValue
 end
