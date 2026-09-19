66939cbf8e21d5fa0cc20f33e7d44705234db4c7
jurreM-dev <269440156+JurreM-dev@users.noreply.github.com>
2026-09-19 08:51:27 +0100

    add int saving function to Saving class to prepare the saves

--- savingSystem/saving.cr
+++ savingSystem/saving.cr
@@ -1,20 +1,24 @@
+alias Save_item = Hash(String, String|Int32)
 class Saving
+  # SAVING 
+  def saveInt(saveKey : String, variable : Int, variableName : String)
+    item = prepSaveInt(variableName, variable)
+    content = prepareSave(saveKey, item)
+  end
 
 
   # HELPER FUNCTIONS
-  def prepareSave(saveName : String, vars)
+  def prepareSave(saveName : String, var)
     saveValue = "[#{saveName}]\n"
-    vars.each do |var|
-      saveValue += "#{var["name"]}:#{var["type"]}=#{var["value"]}\n|{l&l}|\n"
-    end
+    saveValue += "#{var["name"]}:#{var["type"]}=#{var["value"]}\n"
     return saveValue
   end
 
-  def saveInt(name : String, value : Int32)
+  def prepSaveInt(name : String, value : Int32)
     item = Save_item{"type" => "Int", "name" => name, "value" => value}
   end
 
-  def saveString(name : String, value : String)
+  def prepSaveString(name : String, value : String)
     item = Save_item{"type" => "String", "name" => name, "value" => value}
   end
 end
